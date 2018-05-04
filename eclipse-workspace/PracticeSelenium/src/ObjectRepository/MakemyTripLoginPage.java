5be16a4860120f95515c47b03228635cd094fac8
499529 <499529@PC219634.cts.com>
2018-05-04 10:01:42 +0530

    Added make my trip code

---
+++ eclipse-workspace/PracticeSelenium/src/ObjectRepository/MakemyTripLoginPage.java
@@ -0,0 +1,47 @@
+package ObjectRepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class MakemyTripLoginPage {
+	
+WebDriver driver;
+
+
+
+public MakemyTripLoginPage(WebDriver driver)
+{
+	this.driver = driver;
+	PageFactory.initElements(driver, this);
+}
+
+@FindBy(xpath="//*[@id=\"ch_login_email\"]")
+WebElement username;
+
+@FindBy(xpath="//*[@id=\"ch_login_password\"]")
+WebElement password;
+
+@FindBy(xpath="//*[@id ='ch_login_btn']")
+WebElement login;
+
+
+public WebElement Email() {
+	 username.sendKeys("ruchikapriya22@gmail.com");
+	 return username;
+}
+public WebElement Password() {
+	 password.sendKeys("Saibaba@123");
+	 return password;
+	 
+}
+
+public WebElement Login() {
+	login.click();
+	return login;
+
+}
+
+
+}
